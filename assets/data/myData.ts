ab57ac5eb9325c572c05d79141987f48d607b7ed
Belal Kaoukji <b.kaoukji@gmail.com>
2020-08-03 20:54:15 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,9 @@
+import {Kaoukji} from '../../app/kaoukji';
+
+export const MYDATA: Kaoukji = {
+  sNum: 991525168,
+  sName: 'Belal Kaoukji',
+  sAssignment: 'Assignment 4',
+  sCampus: 'Davis',
+  sLogin: 'kaoukji',
+};
